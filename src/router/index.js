d9ebb8ffed6fe3298ea56e5634835f0a67ee2125
Dong Vu <vub1906614@student.ctu.edu.vn>
2023-02-18 21:21:48 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import ContactBook from '../views/ContactBook.vue';
+import NotFound from '../views/NotFound.vue';
+import ContactEdit from '../views/ContactEdit.vue';
 
 const routes = [
     { path: '/', component: ContactBook, name: 'contactbook' },
     // { patch: '/:patchMatch(.*)*', component: NOTFOUND, name: 'notfound' },
     { path: '/:pathMatch(.*)*', name: 'notfound', component: () => import("@/views/NotFound.vue"), },
+    //{ patch: '/contacts/:id', name: 'contact.edit', component: ContactEdit, props: true },
+    { path: '/contacts/:id', name: 'contact.edit', component: () => import("@/views/ContactEdit.vue"), props: true },
 ];
 
 const router = createRouter({
